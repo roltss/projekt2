7ed310a5906ddf4c077a84f2ee741ef755219c8f
Roland Taklai <Roland.Taklai@gmail.com>
2026-04-02 00:43:35 +0300

    commit

---
+++ script.js
@@ -0,0 +1,27 @@
+async function addTodo() {
+  const text = document.getElementById('input').value;
+  const name = prompt('Your name?');
+
+  await fetch('https://tinkr.ee/sdb/my_todos', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ text: text, name: name })
+  });
+
+  document.getElementById('input').value = '';
+  loadTodos();
+}
+
+async function loadTodos() {
+  const response = await fetch('https://tinkr.ee/sdb/my_todos');
+  const todos = await response.json();
+
+  const list = document.getElementById('list');
+  list.innerHTML = '';
+
+  for (const todo of todos) {
+    list.innerHTML += '<li>' + todo.name + ': ' + todo.text + '</li>';
+  }
+}
+
+loadTodos();
